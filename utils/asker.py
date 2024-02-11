7db22025cae4831cef3403137bf7af4c761ab56c
Seynax <seyronax@gmail.com>
2024-02-11 15:02:14 +0100

    asker.ask_dict(...) : ask all data from dict recursively with each {'name': 'default'}

--- utils/asker.py
+++ utils/asker.py
@@ -19,24 +19,45 @@ def value_is_yes(value: str):
 class Asker:
     def __init__(self):
         self.configuration  = {}
-        self.current_dict   = self.current_dict
+        self.dicts          = []
+
+    def get_dict_on_pile(self, index: int):
+        if index < 0 or index > len(self.dicts):
+            return self.configuration
+
+        _dict = self.dicts[index]
+        if _dict is None:
+            return self.configuration
+
+        return _dict
+
+    def current_dict(self):
+        return self.get_dict_on_pile(len(self.dicts) - 1)
+
+    def pop_current_dict(self):
+        if len(self.dicts) > 0:
+            popped = self.dicts.pop()
+            return popped
+        return None
 
     def start_section(self, name: str = None, message: str = None) -> {}:
         message = non_none(message, message_of(name))
         name    = non_blank(name, name_of(message), '_' + str(len(self.configuration)))
         print_color('\033[92m', message)
 
-        self.current_dict   = {}
-        dict_utils.put_if_exists(name, self.configuration, self.current_dict)
-        self.configuration[name] = self.current_dict
+        new_dict = {}
+        dict_utils.put_if_exists(name, self.current_dict(), new_dict)
+        self.current_dict()[name] = new_dict
+        self.dicts.append(new_dict)
 
-        return self.current_dict
+        return self.current_dict()
 
     def make_message(self, name: str = None, default_value: str = None, message: str = None, output_dict: {} = None, *appends: str) -> str:
         message = non_none(message, message_of(name) + ' ?')
 
         for append in appends:
             message += append
+
         message = translate(message, self.configuration)
         message = translate(message, output_dict)
         message = colorize('\033[92m', message)
@@ -48,8 +69,9 @@ class Asker:
         return message
 
     def ask(self, name: str = None, default_value: str = None, message: str = None, output_dict: {} = None):
-        _dict    = non_none(output_dict, self.current_dict)
+        _dict    = non_none(output_dict, self.current_dict(), self.configuration)
 
+        default_value = translate(default_value, self.configuration)
         default_value = translate(default_value, _dict)
         if name in _dict:
             default_value = non_none(_dict[name], default_value)
@@ -64,26 +86,33 @@ class Asker:
 
         return value
 
+    def ask_from_dict(self, ask_dict: {}):
+        for name, value in ask_dict.items():
+            if isinstance(value, Dict):
+                self.start_section(name)
+                self.ask_from_dict(value)
+                self.stop_section()
+                continue
+            self.ask(name, value)
+        return self.configuration
+
     def ask_yes_no(self, name: str = None, default_value: str = None, message: str = None, output_dict: {} = None) -> bool:
-        return value_is_yes(self.ask(name, default_value, self.make_message(name, message, default_value, non_none(output_dict, self.current_dict), ' (y.yes, n.no) ')))
+        return value_is_yes(self.ask(name, default_value, self.make_message(name, message, default_value, non_none(output_dict, self.current_dict()), ' (y.yes, n.no) ')))
 
     def stop_section(self, no_print: bool = False) -> [(str, str, {}), (None, None, None)]:
-        if self.current_dict is None:
-            return None, None, None
-
-        json_settings = jsonify(self.current_dict)
+        json_settings = jsonify(self.current_dict())
         colored_json = highlight(json_settings, lexers.JsonLexer(), formatters.TerminalFormatter())
 
         if not no_print:
             print(colored_json)
 
-        return json_settings, colored_json, self.current_dict
+        return json_settings, colored_json, self.pop_current_dict()
 
     def is_yes(self, name: str) -> bool:
         return value_is_yes(self.get(name))
 
     def get(self, name: str):
-        dict_iterator = self.current_dict
+        dict_iterator = self.current_dict()
         splits = name.split('.')
         if isinstance(splits, List) and len(splits) > 1:
             for i in range(0, len(splits)):
@@ -96,4 +125,4 @@ class Asker:
                     elif isinstance(value, str):
                         return value_is_yes(split)
                     return None
-        return self.current_dict.get(name)
+        return self.current_dict().get(name)
