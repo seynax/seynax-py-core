e945b37d7461fac42a3ddaea14ea993efc024e73
Seynax <seyronax@gmail.com>
2024-02-11 11:16:56 +0100

    attempt to fix setup.py packages

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='seynax_py_core',
     version='1.0.0',
-    packages=['utils'],
-    package_dir={'': 'seynax'},
+    packages=['seynax', 'seynax.utils'],
     url='https://github.com/seynax/seynax-py-core',
     license='GNU General Public License v3.0',
     author='Seynax',
