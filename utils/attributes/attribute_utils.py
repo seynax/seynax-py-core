e6a96355055e05eb05fde2e8d9b7a9ea38c61cfb
Seynax <seyronax@gmail.com>
2024-02-17 07:41:08 +0100

    Attribute attempt call try-except

--- utils/attributes/attribute_utils.py
+++ utils/attributes/attribute_utils.py
@@ -52,51 +52,56 @@ def attempt_call(_callable, parameters: Union[List, Dict, Tuple] = None, forced_
     if _callable is None or not callable(_callable):
         return _callable
 
-    if (parameters is None or len(parameters) == 0) and parent is None and (forced_parameters is None or len(forced_parameters) == 0):
-        return _callable()
+    try:
+        if (parameters is None or len(parameters) == 0) and parent is None and (forced_parameters is None or len(forced_parameters) == 0):
+            return _callable()
 
-    if isinstance(parameters, List):
-        if forced_parameters is not None and isinstance(forced_parameters, List):
-            for forced_parameter in forced_parameters:
-                parameters.append(forced_parameter)
+        if isinstance(parameters, List):
+            if forced_parameters is not None and isinstance(forced_parameters, List):
+                for forced_parameter in forced_parameters:
+                    parameters.append(forced_parameter)
 
-        return _callable(parameters)
-    elif isinstance(forced_parameters, Tuple) or isinstance(parameters, Tuple):
-        return _callable(non_none(forced_parameters, parameters))
+            return _callable(parameters)
+        elif isinstance(forced_parameters, Tuple) or isinstance(parameters, Tuple):
+            return _callable(non_none(forced_parameters, parameters))
 
-    allower = None
-    source  = None
-    if isinstance(parameters, Dict):
-        allower = lambda name: name in parameters
-        source  = lambda name: parameters[name]
-    elif parent is not None:
-        if parameters is not None:
-            allower = lambda name: hasattr(parent, name) and name in parameters
-        else:
-            allower = lambda name: hasattr(parent, name)
-        source  = lambda name: getattr(parent, name)
+        allower = None
+        source  = None
+        if isinstance(parameters, Dict):
+            allower = lambda name: name in parameters
+            source  = lambda name: parameters[name]
+        elif parent is not None:
+            if parameters is not None:
+                allower = lambda name: hasattr(parent, name) and name in parameters
+            else:
+                allower = lambda name: hasattr(parent, name)
+            source  = lambda name: getattr(parent, name)
 
-    signature = inspect.signature(_callable)
-    def attempt_call_with_parameters():
-        final_parameters = non_none(parameters, forced_parameters)
-        if final_parameters is not None and len(final_parameters) > 0:
-            if force_unpack or len(signature.parameters) >= len(final_parameters) and unpack:
-                return _callable(**final_parameters)
-            return _callable(final_parameters)
+        signature = inspect.signature(_callable)
+        def attempt_call_with_parameters():
+            final_parameters = non_none(parameters, forced_parameters)
+            if final_parameters is not None and len(final_parameters) > 0:
+                if force_unpack or len(signature.parameters) >= len(final_parameters) and unpack:
+                    return _callable(**final_parameters)
+                return _callable(final_parameters)
 
-        return _callable()
+            return _callable()
 
-    if allower is None or source is None:
+        if allower is None or source is None:
+            return attempt_call_with_parameters()
+
+        final_parameters = {}
+        if forced_parameters is not None and isinstance(forced_parameters, Dict):
+            for name, value in forced_parameters.items():
+                final_parameters[name] = value
+
+        for name, value in signature.parameters.items():
+            if not allower(name):
+                continue
+            final_parameters[name] = source(name)
+
         return attempt_call_with_parameters()
+    except Exception as e:
+        print(str(e))
 
-    final_parameters = {}
-    if forced_parameters is not None and isinstance(forced_parameters, Dict):
-        for name, value in forced_parameters.items():
-            final_parameters[name] = value
-
-    for name, value in signature.parameters.items():
-        if not allower(name):
-            continue
-        final_parameters[name] = source(name)
-
-    return attempt_call_with_parameters()
+    return _callable
